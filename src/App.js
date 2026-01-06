104c7c1381019f7ddcf348bf671714677a5db55f
angel200306 <angelinayakovleva2003@gmail.com>
2026-01-06 22:13:08 +0200

    Add photo, add Image.js. Change color, add some text and add link another project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react"
+import Image from "./components/Image"
+import logo from "./img/gallery.png"
 
 class App extends React.Component {
     string = "My portfolio site"
     paragraf = "Hello! I'm Noviva. I'm Python Developer. I developed project in Python. And also I can developed web site. I know Python, aiogram, HTML, css, JS and React.js. You can veaw my solo projects cleack “My projects„."
+    string1 = "My solo projects"
     render() {
         return (<div className="name">
             <h1>{this.string}</h1>
             <p>{this.paragraf}</p>
+            <h2>{this.string1}</h2>
+            <a href="https://angel200306.github.io/Gallery/"
+               target="_blank"
+               rel="noopener noreferrer">
+            <Image image={logo} />
+            </a>
         </div>)
     }
 }
