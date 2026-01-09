3fd94463187dc4e08e58601f2b6bffbae91aa6de
angel200306 <angelinayakovleva2003@gmail.com>
2026-01-09 18:53:18 +0200

    Add photo and Image2 for my another project (x2)

---
+++ src/components/Image1.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Image1 extends React.Component {
+    render () {
+        return (
+            <img src={this.props.image1}/>
+        )
+    }
+}
+
+export default Image1
