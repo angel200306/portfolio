db1898c5d3dbc32d73e999403af84baabe1dfae7
angel200306 <angelinayakovleva2003@gmail.com>
2026-01-09 19:42:11 +0200

    Changed text

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react"
 import Image from "./components/Image"
 import logo from "./img/gallery.png"
 import logo1 from "./img/StudyEnglishBot.jpg"
-import logo2 from "./img/Learn Map.png"
+import logo2 from "./img/Drag-Drop Organizer.png"
 
 class App extends React.Component {
     string = "My portfolio site"
-    paragraf = "Hello! I'm Noviva. I am a young programmer who strives to learn new frameworks and improve my skills. I am a front-end developer, and I also know a little bit about back-end development. The purpose of this website is to show you my skills in my pet projects."
+    paragraf = "Hello! I'm Noviva. I am a young programmer who strives to learn new frameworks and improve my skills. I am a front-end developer, and I also know a little bit about back-end development. The purpose of this website is to show you my skills in my pet projects. Disclaimer!!! My pet projects are not commercial at this time. Not all projects may be perfect, but I am still working on them to bring them to perfection"
     string1 = "My solo projects"
     string2 = "My skils"
     paragraf1 = "My skills include HTML, CSS, JS, React, and Flask."
@@ -25,7 +25,7 @@ class App extends React.Component {
                rel="noopener noreferrer">
                <Image image={logo1} />
             </a>
-            <a href="https://angel200306.github.io/Learn-Map/"
+            <a href="https://angel200306.github.io/Drag-And-Drop-oranizer/"
                target="_blank"
                rel="noopener noreferrer">
                 <Image image={logo2} />
