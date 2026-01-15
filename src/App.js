b52622848069aecd3f4cad57d352d46a074ae909
angel200306 <angelinayakovleva2003@gmail.com>
2026-01-15 19:59:37 +0200

    Changed text and add new pet projects

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Image from "./components/Image"
 import logo from "./img/gallery.png"
 import logo1 from "./img/StudyEnglishBot.jpg"
 import logo2 from "./img/Drag-Drop Organizer.png"
+import logo3 from "./img/Brainy Cards.png"
 
 class App extends React.Component {
     string = "My portfolio site"
-    paragraf = "Hello! I'm Noviva. I am a young programmer who strives to learn new frameworks and improve my skills. I am a front-end developer, and I also know a little bit about back-end development. The purpose of this website is to show you my skills in my pet projects. Disclaimer!!! My pet projects are not commercial at this time. Not all projects may be perfect, but I am still working on them to bring them to perfection"
+    paragraf = "Hello! I'm Noviva. I am a young programmer who strives to learn new frameworks and improve my skills. I am a front-end developer, and I also know a little bit about back-end development. The purpose of this website is to show you my skills in my pet projects. Disclaimer!!! My pet projects are not commercial at this time. Not all projects may be perfect, but I am still working on them to bring them to perfection. Written in React: Gallery, Site about my Study English bot, Drag-Drop Organizer. Also written in React my portfolio site which you are reading now."
     string1 = "My solo projects"
     string2 = "My skils"
     paragraf1 = "My skills include HTML, CSS, JS, React, and Flask."
@@ -29,6 +30,11 @@ class App extends React.Component {
                target="_blank"
                rel="noopener noreferrer">
                 <Image image={logo2} />
+            </a>
+            <a href="https://angel200306.github.io/Brainy-Cards/"
+               target="_blank"
+               rel="noopener noreferrer">
+                <Image image={logo3} />
             </a>
             <h2>{this.string2}</h2>
             <p>{this.paragraf1}</p>
---
+++ src/components/Image3.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Image3 extends React.Component {
+    render() {
+        return (
+            <img src={this.props.image3}/>
+        )
+    }
+}
+
+export default Image3
